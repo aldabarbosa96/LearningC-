3117b1522e22a2b9ca743d9b6311ffe2d595c71e
Davidbo <aldabarbosa1996@gmail.com>
2024-05-16 16:46:14 +0200

    primer programa en C++

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//primeras pruebas en C++
+#include <iostream>
+
+int suma(){
+    int num = 0;
+    for (int i = 0; i < 20; i++){
+        for (int  j = 0; j < 15; j++){
+            num++;
+        }
+    }
+    return num;
+}
+
+int main() {
+    std::cout << "Prueba básica de output por terminal" << std::endl; //el endl equivale a hacer \n en java
+    std::cout << "Resultado de la suma: " << suma() << std::endl;
+    return 0;
+}
+
+
+    
